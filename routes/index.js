242d72946ea0a4484fd96602c16beb9a86b3dba9
EarthW0rm <marco.a.faustino@gmail.com>
2018-12-17 20:34:38 -0200

    Incluindo o build da aplicacao simples angular (start e start:prod)

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    //res.render('index', { title: 'Express' });
+    // res.render(path.join(__dirname, '/public', '/index.html'));
+    res.status(200).render(path.join(__dirname, '../public', '/index.html'));
+});
+
+module.exports = router;
